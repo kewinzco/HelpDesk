ab26460bff4080f3bc7544a6272a89fdcc2eb826
richter225 <richter.r020@gmail.com>
2022-11-29 13:01:26 +0100

    test2

--- index.php
+++ index.php
@@ -129,5 +129,54 @@
 </div>
 <script>
 
+        // onkeyup event will occur when the user
+        // release the key and calls the function
+        // assigned to this event
+        function GetDetail(str) {
+            if (str.length == 0) {
+                //document.getElementById("").value = "";
+                //document.getElementById("last_name").value = "";
+                return;
+            }
+            else {
+
+                // Creates a new XMLHttpRequest object
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+
+                    // Defines a function to be called when
+                    // the readyState property changes
+                    if (this.readyState == 4 && this.status == 200) {
+
+                        // Typical action to be performed
+                        // when the document is ready
+                        var myObj = JSON.parse(this.responseText);
+
+                        // Returns the response data as a
+                        // string and store this array in
+                        // a variable assign the value
+                        // received to first name input field
+
+                        document.getElementById("vorname").value = myObj[0];
+
+                        // Assign the value received to
+                        // last name input field
+                        document.getElementById("nachname").value = myObj[1];
+                       document.getElementById("telnr").value = myObj[2];
+                         document.getElementById("büro").value = myObj[3];
+
+                    }
+                };
+
+                // xhttp.open("GET", "filename", true);
+                xmlhttp.open("GET", "include/einfuegen.php", true);
+
+                // Sends the request to the server
+
+                xmlhttp.send();
+            }
+        }
+    </script>
+
 </body>
 </html>
