c0d095da6d5f9ff18a50df7dffd506fd9ef8d57e
richter225 <richter.r020@gmail.com>
2022-12-20 12:43:06 +0100

    neues Form, Achtung speichert so aktuell nicht in DB!!

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
   </head>
   <body>
     <!--form start-->
-    <form action="include/db_speichern.php" method="POST">
+  <!--  <form action="include/db_speichern.php" method="POST">-->
+      <form >
       <div class="container-fluid p-5 bg-primary text-white text-center">
         <h1>Ticket erstellen</h1>
       </div>
