b77fa94e42c13603967804e0fa82c1320e740302
kewinzco <klose.k@gmx.net>
2023-01-24 12:26:25 +0100

    Layout angepasst für Ticket gesendet, Ticket gelöst

--- include/db_speichern.php
+++ include/db_speichern.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Link to Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Link to Bootstrap JS bundle -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 <?php
 // Get the user id
 
@@ -13,12 +18,14 @@ include('config.php');
 try
 {
   $einfuegen -> execute();
+  echo "<div class='container-fluid p-5 bg-primary text-white text-center'><h1>Ticket gesendet</h1></div> <br><br>";
   echo "<h1 style=text-align:center>Ihr Ticket wurde erfolgreich entgegengenommen.<br>";
   echo "Bitte warten Sie darauf dass ein Mitarbeiter Sie kontaktiert.</h1>";
 
 }
 catch(Exception $e)
 {
+    echo "<div class='container-fluid p-5 bg-primary text-white text-center'><h1>Ticket Fehler</h1></div> <br><br>";
     echo "<h1 style=text-align:center>Fehler beim Absenden vom Ticket. <br>";
     echo "Bitte kontaktieren Sie Matthias Feil unter Folgender EMail-Adresse: matthias.feil@hof-university.de </h1> <br> ";
     echo  "Fehlermeldung: $e -> getMessage()";
